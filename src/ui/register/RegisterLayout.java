9ff486fb119ac0e7b13058cb21068c592cd27900
mxdlzg <mxdlzg@163.com>
2017-06-19 16:56:38 +0800

    Initial commit

---
+++ src/ui/register/RegisterLayout.java
@@ -0,0 +1,31 @@
+/*
+ * Created by JFormDesigner on Mon Jun 19 16:44:41 CST 2017
+ */
+
+package ui.register;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * @author unknown
+ */
+public class RegisterLayout extends JFrame {
+    public RegisterLayout() {
+        initComponents();
+    }
+
+    private void initComponents() {
+        // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
+
+        //======== this ========
+        Container contentPane = getContentPane();
+        contentPane.setLayout(new FlowLayout());
+        pack();
+        setLocationRelativeTo(getOwner());
+        // JFormDesigner - End of component initialization  //GEN-END:initComponents
+    }
+
+    // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
+    // JFormDesigner - End of variables declaration  //GEN-END:variables
+}
